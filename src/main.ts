908efa4ec3ee4320a33a4b97ccadb02613ed2abe
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-07-07 10:19:23 +0100

    final review

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
-let state = {
+type Brewery = {
+  id: string
+  name: string
+  brewery_type: string
+  street: string | null
+  address_2: string | null
+  address_3: string | null
+  city: string
+  state: string
+  county_province: string | null
+  postal_code: string
+  country: string
+  longitude: string | null
+  latitude: string | null
+  phone: string | null
+  website_url: string | null
+  updated_at: string
+  created_at: string
+}
+
+type State = {
+  USState: string
+  search: string
+  breweries: Brewery[]
+}
+
+let state: State = {
   USState: '',
+  search: '',
   breweries: []
 }
 
@@ -7,45 +34,171 @@ let state = {
 // Q: What breweries do we need to display? state.breweries
 
 function getBreweriesForState () {
-  // find breweries in this state
-  // put them in state
-  // rerender
+  fetch(
+    `https://api.openbrewerydb.org/breweries?by_state=${state.USState}&per_page=10`
+  )
+    .then(resp => resp.json())
+    .then(breweries => {
+      state.breweries = breweries
+      render()
+    })
+}
+
+function getSearchedBreweries () {
+  fetch(
+    `https://api.openbrewerydb.org/breweries?by_name=${state.search}&by_state=${state.USState}`
+  )
+    .then(resp => resp.json())
+    .then(breweries => {
+      state.breweries = breweries
+      render()
+    })
 }
 
 function renderHeader () {
-  // <h1>List of Breweries</h1>
-  // <header class="search-bar">
-  //   <form id="search-breweries-form" autocomplete="off">
-  //     <label for="search-breweries"><h2>Search breweries:</h2></label>
-  //     <input id="search-breweries" name="search-breweries" type="text" />
-  //   </form>
-  // </header>
+  let titleEl = document.createElement('h1')
+  titleEl.textContent = 'List of Breweries'
+
+  let searchBarHeader = document.createElement('header')
+  searchBarHeader.className = 'search-bar'
+
+  searchBarHeader.append(titleEl)
+
+  let searchBreweriesForm = document.createElement('form')
+  searchBreweriesForm.id = 'search-breweries-form'
+  searchBreweriesForm.autocomplete = 'off'
+  searchBreweriesForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    // find and and display only the breweries from the current state
+    // that match our search
+
+    // Strategy 1: Get them from the server
+    // https://api.openbrewerydb.org/breweries?by_name=brew&by_state=texas
+    state.search = searchBreweriesInput.value
+    getSearchedBreweries()
+
+    // Stragegy 2 : Filter the breweries we have in state
+  })
+
+  let searchBreweriesLabel = document.createElement('label')
+  searchBreweriesLabel.htmlFor = 'search-breweries'
+
+  let searchBreweriesH2 = document.createElement('h2')
+  searchBreweriesH2.textContent = 'Search breweries:'
+
+  let searchBreweriesInput = document.createElement('input')
+  searchBreweriesInput.id = 'search-breweries'
+  searchBreweriesInput.name = 'search-breweries'
+  searchBreweriesInput.type = 'text'
+
+  searchBreweriesLabel.append(searchBreweriesH2)
+  searchBreweriesForm.append(searchBreweriesLabel, searchBreweriesInput)
+  searchBarHeader.append(searchBreweriesForm)
+
+  return searchBarHeader
+}
+
+function createMessageH2 (text: string) {
+  let messageEl = document.createElement('h2')
+  messageEl.className = 'message'
+  messageEl.textContent = text
+
+  return messageEl
 }
 
 function renderBreweryList () {
-  // <article>
-  //   <ul class="breweries-list">
-  //   </ul>
-  // </article>
+  if (state.breweries.length === 0) {
+    let messageEl = createMessageH2(
+      `No breweries found for ${state.search} in ${state.USState}.`
+    )
+    return messageEl
+  } else {
+    let articleEl = document.createElement('article')
+
+    let breweriesUl = document.createElement('ul')
+    breweriesUl.className = 'breweries-list'
+
+    for (let brewery of state.breweries) {
+      let liEl = createSingleBreweryLi(brewery)
+      breweriesUl.append(liEl)
+    }
+
+    articleEl.append(breweriesUl)
+    return articleEl
+  }
 }
 
-function renderSingleBrewery () {
-  //     <li>
-  //       <h2>Snow Belt Brew</h2>
-  //       <div class="type">micro</div>
-  //       <section class="address">
-  //         <h3>Address:</h3>
-  //         <p>9511 Kile Rd</p>
-  //         <p><strong>Chardon, 44024</strong></p>
-  //       </section>
-  //       <section class="phone">
-  //         <h3>Phone:</h3>
-  //         <p>N/A</p>
-  //       </section>
-  //       <section class="link">
-  //         <a href="null" target="_blank">Visit Website</a>
-  //       </section>
-  //     </li>
+// Previous:
+// input: brewery, list
+// action: creates an li and appends it to the list
+// output: undefined
+
+// Current:
+// input: brewery
+// action: creates an li
+// output: li
+function createSingleBreweryLi (brewery: Brewery) {
+  let breweryLi = document.createElement('li')
+
+  let breweryTitle = document.createElement('h2')
+  breweryTitle.textContent = brewery.name
+
+  let breweryTypeDiv = document.createElement('div')
+  breweryTypeDiv.className = 'type'
+  breweryTypeDiv.textContent = brewery.brewery_type
+
+  let breweryAddressSection = document.createElement('section')
+  breweryAddressSection.className = 'address'
+
+  let breweryAddressTitle = document.createElement('h3')
+  breweryAddressTitle.textContent = 'Address:'
+
+  let breweryAddressLine1 = document.createElement('p')
+  breweryAddressLine1.textContent = brewery.street
+
+  let breweryAddressLine2 = document.createElement('p')
+
+  let breweryAddressLine2Strong = document.createElement('strong')
+  breweryAddressLine2Strong.textContent = `${brewery.city}, ${brewery.postal_code}`
+
+  let breweryPhoneSection = document.createElement('section')
+  breweryPhoneSection.className = 'phone'
+
+  let breweryPhoneTitle = document.createElement('h3')
+  breweryPhoneTitle.textContent = 'Phone:'
+
+  let breweryPhoneP = document.createElement('p')
+  breweryPhoneP.textContent = brewery.phone ? brewery.phone : 'N/A'
+
+  let breweryLinkSection = document.createElement('section')
+  breweryLinkSection.className = 'link'
+
+  let breweryLinkA = document.createElement('a')
+  if (brewery.website_url) {
+    breweryLinkA.href = brewery.website_url ? brewery.website_url : '#'
+    breweryLinkA.target = '_blank'
+    breweryLinkA.textContent = 'Visit Website'
+  } else {
+    breweryLinkA.textContent = 'No Website'
+  }
+
+  breweryLi.append(
+    breweryTitle,
+    breweryTypeDiv,
+    breweryAddressSection,
+    breweryPhoneSection,
+    breweryLinkSection
+  )
+  breweryAddressSection.append(
+    breweryAddressTitle,
+    breweryAddressLine1,
+    breweryAddressLine2
+  )
+  breweryAddressLine2.append(breweryAddressLine2Strong)
+  breweryPhoneSection.append(breweryPhoneTitle, breweryPhoneP)
+  breweryLinkSection.append(breweryLinkA)
+
+  return breweryLi
 }
 
 function render () {
@@ -53,8 +206,10 @@ function render () {
   if (mainEl === null) return
   mainEl.textContent = ''
 
-  renderHeader()
-  renderBreweryList()
+  let headerEl = renderHeader()
+  let listEl = renderBreweryList()
+
+  mainEl.append(headerEl, listEl)
 }
 
 function listenToSelectStateForm () {
@@ -68,3 +223,6 @@ function listenToSelectStateForm () {
 }
 
 listenToSelectStateForm()
+
+// Step 1: Prevent the search form from refreshing the page ✅
+// Step 2: Trigger some action when the form submits ✅
